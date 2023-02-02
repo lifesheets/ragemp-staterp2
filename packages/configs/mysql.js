40d55d03760408bb75f6314061700f58cebb0be4
lifesheets <97382411+lifesheets@users.noreply.github.com>
2023-02-02 15:57:44 +0200

    [Up] Updated configuration for MySQL mode.

---
+++ packages/configs/mysql.js
@@ -0,0 +1,11 @@
+"use strict"; // MySQL Connect (Created by ua.lifesheets on 02.02.2023).
+
+let config = {
+    'Host': '127.0.0.1',
+    'Port': 3306,
+    'Name': 'database',
+    'User': 'username',
+    'Pass': 'password'
+}
+
+module.exports = config;
